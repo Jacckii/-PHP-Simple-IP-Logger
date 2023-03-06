294af3f17fccbbb34ffa4401b9dcf0ef6f173b72
Clément PAPPALARDO <16377344+lemassykoi@users.noreply.github.com>
2023-03-06 05:41:51 +0100

    Update logger.php
    updated browser and os detection, with get_browser from https://www.php.net/manual/en/function.get-browser.php (please read NOTES at bottom)
    added missing ```/``` before file name

--- logger.php
+++ logger.php
@@ -4,91 +4,12 @@ if (!isset($vist_page))
 	$vist_page     =   "logger.php";
 }
 $user_agent     =   $_SERVER['HTTP_USER_AGENT'];
-// Getting OS Name
-function getOS() { 
 
-    global $user_agent;
+// YOU NEED TO CONFIGURE BROWSCAP FOR THIS TO WORK
+$browser        =   get_browser($user_agent, true);
+$user_os        =   $browser['platform'];
+$user_browser   =   $browser['browser'];
 
-    $os_platform    =   "Unknown OS Platform";
-
-    $os_array       =   array(
-                            '/windows nt 10/i'     =>  'Windows 10',
-                            '/windows nt 6.3/i'     =>  'Windows 8.1',
-                            '/windows nt 6.2/i'     =>  'Windows 8',
-                            '/windows nt 6.1/i'     =>  'Windows 7',
-                            '/windows nt 6.0/i'     =>  'Windows Vista',
-                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-                            '/windows nt 5.1/i'     =>  'Windows XP',
-                            '/windows xp/i'         =>  'Windows XP',
-                            '/windows nt 5.0/i'     =>  'Windows 2000',
-                            '/windows me/i'         =>  'Windows ME',
-                            '/win98/i'              =>  'Windows 98',
-                            '/win95/i'              =>  'Windows 95',
-                            '/win16/i'              =>  'Windows 3.11',
-                            '/macintosh|mac os x/i' =>  'Mac OS X',
-                            '/mac_powerpc/i'        =>  'Mac OS 9',
-                            '/linux/i'              =>  'Linux',
-							'/kalilinux/i'          =>  'KaliLinux',
-                            '/ubuntu/i'             =>  'Ubuntu',
-                            '/iphone/i'             =>  'iPhone',
-                            '/ipod/i'               =>  'iPod',
-                            '/ipad/i'               =>  'iPad',
-                            '/android/i'            =>  'Android',
-                            '/blackberry/i'         =>  'BlackBerry',
-                            '/webos/i'              =>  'Mobile',
-							'/Windows Phone/i'      =>  'Windows Phone'
-                        );
-
-    foreach ($os_array as $regex => $value) { 
-
-        if (preg_match($regex, $user_agent)) {
-            $os_platform    =   $value;
-        }
-
-    }   
-
-    return $os_platform;
-
-}
-// END of Getting OS
-//
-// Get browser
-function getBrowser() {
-
-    global $user_agent;
-
-    $browser        =   "Unknown Browser";
-
-    $browser_array  =   array(
-                            '/msie/i'       =>  'Internet Explorer',
-                            '/firefox/i'    =>  'Firefox',
-							'/Mozilla/i'	=>	'Mozila',
-							'/Mozilla/5.0/i'=>	'Mozila',
-                            '/safari/i'     =>  'Safari',
-                            '/chrome/i'     =>  'Chrome',
-                            '/edge/i'       =>  'Edge',
-                            '/opera/i'      =>  'Opera',
-							'/OPR/i'        =>  'Opera',
-                            '/netscape/i'   =>  'Netscape',
-                            '/maxthon/i'    =>  'Maxthon',
-                            '/konqueror/i'  =>  'Konqueror',
-							'/Bot/i'		=>	'BOT Browser',
-							'/Valve Steam GameOverlay/i'  =>  'Steam',
-                            '/mobile/i'     =>  'Handheld Browser'
-                        );
-
-    foreach ($browser_array as $regex => $value) { 
-
-        if (preg_match($regex, $user_agent)) {
-            $browser    =   $value;
-        }
-
-    }
-
-    return $browser;
-
-}
-// END of getting browser
 function getIP() 
 { 
 	if (isset($_SERVER)) 
@@ -163,6 +84,6 @@ if(!isset($_SESSION[$vist_page]) || $_SESSION[$vist_page]!=1)
 		$country = $details->country;
 	}
 	// Writing in to txt file
-	file_put_contents(__DIR__ . 'iplist.txt',date("Y-m-d - H:i:s - "). $country. " ". $ip." | ". $vist_page." | ". $user_os. " | ". $user_browser. " | From: ". $site. " | user agent:" .$user_agent .PHP_EOL , FILE_APPEND | LOCK_EX);
+	file_put_contents(__DIR__ . '/iplist.txt',date("Y-m-d - H:i:s - "). $country. " ". $ip." | ". $vist_page." | ". $user_os. " | ". $user_browser. " | From: ". $site. " | user agent:" .$user_agent .PHP_EOL , FILE_APPEND | LOCK_EX);
 }
 ?>
